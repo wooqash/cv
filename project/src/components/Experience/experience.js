908ee73e194828349a3b4d9b5275b70614e71b34
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-10-26 07:27:50 +0200

    Add section experience

--- project/src/components/Experience/experience.js
+++ project/src/components/Experience/experience.js
@@ -1,12 +1,24 @@
 import React from "react";
-import siteConfig from "../../../data/config";
 
 import "./experience.scss";
 
-const Experience = (props) => (
+const Experience = (props) => {
+    return (
     <section className="section experience">
-        <h3 className="section__title">Section title</h3>
+        <h3 className="section__title">{props.title}</h3>
+        {
+            props.employments.map((employement, index) => {
+                return (
+                    <div className="employment" key={index}>
+                        <h4 className="employment__company">{employement.company}</h4>
+                        <p className="employment__date">{employement.date}</p>
+                        <p className="employment__position">{employement.position}</p>
+                        <p className="employment__responsibilities">{employement.responsibilities}</p>
+                    </div>
+                )
+            })
+        }
     </section>
-);
+)};
 
 export default Experience;
