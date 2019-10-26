23c5f320277817408ad3fe3b700fd8996b1d0dbb
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-10-26 07:28:13 +0200

    Add section education

--- project/src/components/Education/education.js
+++ project/src/components/Education/education.js
@@ -1,11 +1,22 @@
 import React from "react";
-import siteConfig from "../../../data/config";
 
 import "./education.scss";
 
 const Education = (props) => (
     <section className="section education">
-        <h3 className="section__title">Section title</h3>
+        <h3 className="section__title">{props.title}</h3>
+        {
+            props.schools.map((school, index) => {
+                return (
+                    <div className="school" key={index}>
+                        <h4 className="school__name">{school.name}</h4>
+                        <p className="school__date">{school.date}</p>
+                        <p className="school__field">{school.field}</p>
+                        <p className="school__spec">{school.spec}</p>
+                    </div>
+                )
+            })
+        }
     </section>
 );
 
