301f99f75d51f9839a149926d9b32381a8d190a9
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-10-26 07:28:41 +0200

    Add section skills

--- project/src/components/Skills/skills.js
+++ project/src/components/Skills/skills.js
@@ -1,13 +1,31 @@
 import React from "react";
-import siteConfig from "../../../data/config";
 
 import "./skills.scss";
 
-const Skills = (props) => (
-    <section className="section skills">
-        <h3 className="section__title">Section title</h3>
-        <p>Lorem ipsum </p>
-    </section>
-);
+const Skills = (props) => {
+    return (
+        <section className="section skills">
+            <h3 className="section__title">{props.title}</h3>
+            <ul className="skill-group">
+                {
+                    props.skills.map((skill, index) => {
+                        return (
+                            <li className="skill-group__item" key={index}>
+                                <h4 className="skill-group-name">{skill.name}</h4>
+                                <ul className="skill-group-list">
+                                { skill.items.map((item, index) => {
+
+                                        return(<li className={item.marked ? "skill-group-list__item skill-group-list__item--marked": "skill-group-list__item"} key={index}>{item.name}</li>);
+                                    })
+                                }
+                                </ul>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
+    );
+}
 
 export default Skills;
