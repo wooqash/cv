09e9e0e0ca4ca17dc072f8c4415dd374d838817a
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-10-29 06:46:35 +0100

    Changes in layout

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { injectIntl, intlShape, FormattedMessage } from "react-intl";
+import { injectIntl, intlShape } from "react-intl";
 import { compose } from "recompose";
 import siteConfig from "../../data/config";
 
-import { Container, Row, Col } from "react-awesome-styled-grid";
+import { Container, Row, Col, ScreenBadge } from "react-awesome-styled-grid";
 
 import withPageContext from "../pageContext";
 import withLayout from "../layout";
@@ -26,32 +26,33 @@ const IndexPage = (props) => {
     const { keywords } = siteConfig;
     const { locale } = props.pageContext;
     const { ...translations } = props.intl.messages;
-    console.log(translations);
     return (
         <React.Fragment>
             <SEO title={title} keywords={keywords} lang={locale}  />
-            <main>
-                <Container className="page-content" fluid>
-                    <Row>
-                        <Col xs={4}>
-                            <Hero></Hero>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={4} md={4} className="column column--left">
-                            <Experience title={translations.experience.title} employments={translations.experience.employments}></Experience>
-                            <Education title={translations.education.title} schools={translations.education.schools}></Education>
-                        </Col>
-                        <Col xs={4} md={4} className="column column--rigth">
-                            <About title={translations.about.title} desc={translations.about.desc}></About>
-                            <Skills title={translations.skills.title} skills={translations.skills.skills}></Skills>
-                            <Contact title={translations["contact.title"]}></Contact>
-                        </Col>
-                    </Row>
-                </Container>
-                <p>{locale}</p>
-                <FormattedMessage id="home.main" tagName="div" />
-            </main>
+            <div className="page-wrapper">
+                <main>
+                    <Container className="page-content" fluid>
+                        <Row>
+                            <Col xs={4}>
+                                <Hero></Hero>
+                            </Col>
+                        </Row>
+                        <Row className="row main-content">
+                            <Col reverse={['md']}  xs={4} md={4} className="column column--left">
+                                <Experience title={translations.experience.title} employments={translations.experience.employments}></Experience>
+                                <Education title={translations.education.title} schools={translations.education.schools}></Education>
+                            </Col>
+                            <Col  xs={4} md={4} className="column column--rigth">
+                                <About title={translations.about.title} desc={translations.about.desc}></About>
+                                <Skills title={translations.skills.title} skills={translations.skills.skills}></Skills>
+                                <Contact title={translations["contact.title"]}></Contact>
+                            </Col>
+                        </Row>
+                    </Container>
+                </main>
+                <div className="briefcase"></div>
+            </div>
+            <ScreenBadge className="debug"/>
         </React.Fragment>
     );
 };
