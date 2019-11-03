31f2ba999b5bae2fd82d3625c5b6b07ba2a2e9ca
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-11-03 09:41:21 +0100

    Add footer and english content

--- project/src/components/Footer/Footer.jsx
+++ project/src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import siteConfig from "../../../data/config";
 
-const Footer = () => (
-    <footer>
-        <FormattedMessage id="footer.title" />
-    </footer>
-);
+import "./footer.scss";
+
+const Footer = () => {
+    const startDate = '2019';
+    const date = new Date().getFullYear().toString();
+    return (<footer>
+        {siteConfig.authorName} <span>&copy;</span> <span>{date !== startDate ? startDate + ' - ' + date : date  }</span>, <FormattedMessage id="footer.title" />
+    </footer>)
+};
 
 export default Footer;
